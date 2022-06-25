bcdb26b33a9e8ae40b62633662274ec454e39acd
eneskacan <enes.kacan@agu.edu.tr>
2022-06-25 23:22:57 +0300

    Add custom plugin

---
+++ src/main/java/com/eneskacan/summarizer_maven_plugin/Summarizer.java
@@ -0,0 +1,16 @@
+package com.eneskacan.summarizer_maven_plugin;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+
+@Mojo(name = "summarize", defaultPhase = LifecyclePhase.COMPILE)
+public class Summarizer extends AbstractMojo {
+
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+
+    }
+}
